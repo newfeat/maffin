63f06f9ddad3ef59a590945b5bc61a1661e74f71
Nadin Kasikova <ivanov@example.com>
2017-06-07 18:49:27 +0300

    Second

--- protected/Models/User.php
+++ protected/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../Model.php';
+//require __DIR__ . '/../Model.php';
 
 class User
     extends Model
--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/../protected/Db.php';
 require __DIR__ . '/../protected/Models/Product.php';
-require __DIR__ . '/../protected/Models/User.php';
+//require __DIR__ . '/../protected/Models/User.php';
 
 
 $data = Product::findAll();
